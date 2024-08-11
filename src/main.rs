62ad245eeb57c4b7f80545e4f5bc081ea47dd2a7
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2024-08-11 12:01:57 +0200

    Started with Input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::io::{stdout, Result};
-
+// A Weather App in the terminal
 use ratatui::{
     backend::CrosstermBackend,
     crossterm::{
@@ -10,22 +9,63 @@ use ratatui::{
     style::Stylize,
     widgets::Paragraph,
     Terminal,
+    prelude::*,
 };
 
+use std::io::{stdout, Result, Write};
+use tui_input::backend::crossterm as backend;
+use tui_input::backend::crossterm::EventHandler;
+use tui_input::Input;
+use ratatui::widgets::Wrap;
+
 fn main() -> Result<()> {
+
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    let stdout_var = stdout();
+    let mut stdout_var = stdout_var.lock();
     terminal.clear()?;
+
+    let mut input: Input = "Basel".into();
+    backend::write(&mut stdout_var, input.value(), input.cursor(), (0,0), 15)?;
+    stdout_var.flush()?;
 
     loop {
         terminal.draw(|frame| {
-            let area = frame.area();
+            let outer_layout = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(0)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Min(2),
+                        Constraint::Min(0),
+                        Constraint::Length(1),
+                    ]
+                        .as_ref(),
+                )
+                .split(frame.size());
+
             frame.render_widget(
-                Paragraph::new("Weather App! (press 'q' to quit)")
-                    .white()
-                    .on_blue(),
-                area,
+                Paragraph::new("Weather App")
+                    .style(Style::default().fg(Color::White).bg(Color::Blue))
+                    .alignment(Alignment::Center),
+                outer_layout[0],
+            );
+
+            frame.render_widget(
+                Paragraph::new("Press 'q' to exit")
+                    .style(Style::default().fg(Color::White).bg(Color::Blue))
+                    .alignment(Alignment::Left),
+                outer_layout[3],
+            );
+
+            frame.render_widget(
+                Paragraph::new("Weather Information")
+                    .style(Style::default().fg(Color::White).bg(Color::Blue))
+                    .alignment(Alignment::Center),
+                outer_layout[2],
             );
         })?;
 
